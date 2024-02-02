553011ccde0de5aa8014373dbd02d9941eae47af
Nikita <xstratumm@yandex.ru>
2024-02-02 16:33:51 +0300

    Функция 3. Найти произведение таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа.

--- 1/task.py
+++ 1/task.py
@@ -23,13 +23,34 @@ def sum_prime_divs(num):
     # return divs
     return sum(divs)
 
-# 2
-def count_odd_digits(num):
-    return len([str(num)[i] for i in range(len(str(num))) if int(str(num)[i]) % 2 and int(str(num)[i]) > 3])
-
 # 1 test
 # for n in range(1, 30):
 #     print(str(n) + ": " + str(sum_prime_divs(n)))
 
+# 2
+def count_odd_digits(num):
+    return len([str(num)[i] for i in range(len(str(num))) if int(str(num)[i]) % 2 and int(str(num)[i]) > 3])
+
 # 2 test
-print(count_odd_digits(123456789))
+# print(count_odd_digits(123456789))
+
+def digits_sum(num):
+    return sum([int(str(num)[i]) for i in range(len(str(num)))])
+
+# 3
+def prod_divs(num):
+    num_digits_sum = digits_sum(num)
+    divs = [1]
+
+    for div in range(2, num):
+        if not num % div and digits_sum(div) < num_digits_sum:
+            divs += [div]
+    
+    prod = 1
+    for div in divs:
+        prod *= div
+    print(divs)
+    return prod
+
+# 3 test
+print(prod_divs(23454))
