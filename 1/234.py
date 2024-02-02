556f4d4af3aa8c530653251d714ab7bb37f7a30e
Nikita <xstratumm@yandex.ru>
2024-02-02 21:23:06 +0300

    fix task_9

--- 1/234.py
+++ 1/234.py
@@ -5,17 +5,19 @@ def task_1(string):
     return len(list(filter(lambda symb: True if ord(symb) in list(range(1072, 1103+1)) + [1105,] else False, list(string))))
 
 # tested on 'kfk', 'kffk', 'kfgk'
+# abdABdba
 def task_9(string):
     for i in range(len(string)):
-        if string[i] != string[-1 - i]:
-            return "nope"
+        # isalpha() - check Latin
+        # if both symmetric letters are in uppercase or not Latin, it's ok
+        if string[i] != string[-1 - i] and (string[i].islower() and string[i].isalpha() or string[-1 - i].islower() and string[-1 - i].islower()):
+                return "nope"
     return "yup"
 
 # tested on kfk31.03.2034gkgkfk36.05.2034gkgkfk02.12.2034gkg
 # kfk31.03.2034gkgkfk36.05.2034gkgkfk02.12.20
 # 1.03.2034gkgkfk36.05.2034gkgkfk02.12.2034kgk
 def task_18(string):
-    # если не нашел, не отрезаю
     dates = []
     for month in ("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"):
         index = string.find("." + month + ".")
