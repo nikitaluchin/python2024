2a3edaab82369e3c0fd928023a9832c726df4ac1
Nikita <xstratumm@yandex.ru>
2024-02-02 20:56:56 +0300

    18. Найти в тексте даты формата «день.месяц.год».

--- 1/234.py
+++ 1/234.py
@@ -11,6 +11,20 @@ def task_9(string):
             return "nope"
     return "yup"
 
+# tested on kfk31.03.2034gkgkfk36.05.2034gkgkfk02.12.2034gkg
+# kfk31.03.2034gkgkfk36.05.2034gkgkfk02.12.20
+# 1.03.2034gkgkfk36.05.2034gkgkfk02.12.2034kgk
+def task_18(string):
+    # если не нашел, не отрезаю
+    dates = []
+    for month in ("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"):
+        index = string.find("." + month + ".")
+        if index == -1:
+            continue
+        if string[index-2:index].isnumeric() and int(string[index-2:index]) < 32 and string[index+4:index+8].isnumeric() and len(string[index+4:index+8]) == 4:
+            dates += [string[index-2:index+8]]
+            string = string.replace(string[index-2:index+8], "")
+    return dates
 
 while True:
     task_num = input("Enter task num from (1, 9, 18) >> ")
