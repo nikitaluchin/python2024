f425fee2f73b6bd58bd42787a0e499d908ffc9f6
Nikita <xstratumm@yandex.ru>
2024-02-03 12:03:59 +0300

    Задание 10. Дан список строк с клавиатуры. Упорядочить по количеству слов в строке.

---
+++ 1/10.py
@@ -0,0 +1,16 @@
+n = int(input("Enter amount of lines >> "))
+lines = [input() for i in range(n)]
+
+# 6
+# aa a aa aaa  
+# aaaa
+# a aaa
+# aaa aa aa aa aa aaa
+# aa 
+# aaaa aa aaa
+for i in range(n - 1):
+    for j in range(i + 1, n):
+        if len(lines[i].split()) > len(lines[j].split()):
+            lines[i], lines[j] = lines[j], lines[i]
+
+print(lines)
