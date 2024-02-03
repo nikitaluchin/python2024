5a0644b1427569f195147ee3866070d8ac0b154d
Nikita <xstratumm@yandex.ru>
2024-02-03 22:46:35 +0300

    8. В порядке увеличения квадратичного отклонения между средним весом ASCII-кода символа в строке и максимально среднего ASCII-кода тройки подряд идущих символов в строке.

--- 1/11-14.py
+++ 1/11-14.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def count_diff(line):
     vowels_amount, consonants_amount = 0, 0
 
@@ -16,12 +18,32 @@ def count_diff(line):
 def task_1():
     n = int(input("Enter amount of lines >> "))
     lines = [input() for i in range(n)]
-    print(list(map(count_diff, lines)))
+    # print(list(map(count_diff, lines)))
 
     for i in range(n - 1):
         for j in range(i + 1, n):
             if count_diff(lines[i]) > count_diff(lines[j]):
                 lines[i], lines[j] = lines[j], lines[i]
+    return lines
+
+# sd = sqrt(1/n * Σi=1,n (xi-\overline{x})^2),
+# \overline{x} = 1/n * Σi=1,n (xi), where xi - ASCII code
+# tested on
+# fjfj ld djdjls
+# abafk flff
+# gkgkg nanas
+def task_4():
+    n = int(input("Enter amount of lines >> "))
+    lines = [input() for i in range(n)]
+    avg = (1/len(lines[0])) * sum([ord(lines[0][i]) for i in range(len(lines[0]))])
+    # print(avg)
+
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if sqrt((1/len(lines[i])) * sum([(ord(lines[i][k]) - avg) ** 2 for k in range(len(lines[i]))])) \
+               > sqrt((1/len(lines[j])) * sum([(ord(lines[j][k]) - avg) ** 2 for k in range(len(lines[j]))])):
+                lines[i], lines[j] = lines[j], lines[i]
+                # print(sqrt((1/len(lines[i])) * sum([(ord(lines[i][k]) - avg) ** 2 for k in range(len(lines[i]))])))
     return lines
 
 while True:
