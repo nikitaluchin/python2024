7323e6251ace91ce4bb8656434a7500a7fd7c5ce
Nikita <xstratumm@yandex.ru>
2024-02-04 08:54:05 +0300

    19. Дан целочисленный массив. Необходимо осуществить циклический сдвиг элементов массива вправо на одну позицию.

--- 1/15-19.py
+++ 1/15-19.py
@@ -5,6 +5,11 @@ def task_7(l):
             l[i] = l_copy[i - 1]
     return l
 
+def task_19(l):
+    l_copy = l.copy()
+    for i in range(len(l)):
+        l[i] = l_copy[i - 1]
+    return l
 
 while True:
     task_num = input("Enter task num from (7, 19, 31, 43, 55) >> ")
@@ -12,7 +17,7 @@ while True:
     if task_num == "7":
         print(task_7([1,2,3,4,5]))
     elif task_num == "19":
-        print(task_19())
+        print(task_19([1,2,3,4,5]))
     elif task_num == "31":
         print(task_31())
     elif task_num == "43":
