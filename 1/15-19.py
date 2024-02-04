26ae89493acb489e1a2d1004a99c242418cc43ec
Nikita <xstratumm@yandex.ru>
2024-02-04 09:04:53 +0300

    31. Дан целочисленный массив. Найти количество чётных элементов.

--- 1/15-19.py
+++ 1/15-19.py
@@ -11,6 +11,18 @@ def task_19(l):
         l[i] = l_copy[i - 1]
     return l
 
+def task_31(l):
+    return len(list(filter(lambda el: True if el % 2 == 0 else False, l)))
+
+    # count = 0
+    # for el in l:
+    #     if el % 2 == 0:
+    #         count += 1
+    # return count
+
+# def task_42(l):
+    # return l.count(min(l))
+
 while True:
     task_num = input("Enter task num from (7, 19, 31, 43, 55) >> ")
 
@@ -19,8 +31,8 @@ while True:
     elif task_num == "19":
         print(task_19([1,2,3,4,5]))
     elif task_num == "31":
-        print(task_31())
+        print(task_31([1,2,3,4,5]))
     elif task_num == "43":
-        print(task_43())
+        print(task_43([1,3,6,1,7,1,8,1]))
     elif task_num == "55":
         print(task_55())
