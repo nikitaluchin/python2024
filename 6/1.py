60fa6a187a2321cc2793230b3b458fa655e0259d
nikitaluchin <luchinnightwolf543@gmail.com>
2024-02-18 13:33:29 +0300

    Задание 4. Написать не менее трех статистических запросов (SELECT).

--- 6/1.py
+++ 6/1.py
@@ -9,6 +9,70 @@ con = sqlite3.connect("hospital.db")
 cur = con.cursor()
 script_file = open("script.sql")
 cur.executescript(script_file.read())
+con.commit()
 
-# con.commit()
-# con.close()
+# Выбрать имена всех пациентов, их возраст и диагноз,
+# а также имена врачей, которые надзират за ними
+query = """SELECT 
+    p.first_name || ' ' || p.surname,
+    pat.age,
+    pat.diagnosis,
+    d.first_name || ' ' || d.surname
+FROM 
+    patient pat
+JOIN 
+    person p ON pat.person_id = p.id
+JOIN 
+    staff s ON pat.supervised_by = s.person_id
+JOIN 
+    person d ON s.person_id = d.id
+WHERE 
+    s.staff_type_id = 2; -- Выбираем только врачей
+"""
+
+cur.execute(query)
+for res in cur.fetchall():
+    print(res)
+print()
+
+# Подсчитать количество пациентов, находящихся под наблюдением
+# каждого врача
+query = """SELECT 
+    p.first_name || ' ' || p.surname AS doctor_name,
+    COUNT(pat.id)
+FROM 
+    staff s
+JOIN 
+    person p ON s.person_id = p.id
+JOIN 
+    patient pat ON s.person_id = pat.supervised_by
+WHERE 
+    s.staff_type_id = 2 -- Выбираем только врачей
+GROUP BY
+    doctor_name;
+"""
+
+cur.execute(query)
+for res in cur.fetchall():
+    print(res)
+print()
+
+# Выбрать всех членов персонала, отсортированных по типу персонала по убыванию
+query = """SELECT 
+    p.first_name || ' ' || p.surname,
+    st.type_name
+FROM
+    staff s
+JOIN 
+    person p ON s.person_id = p.id
+JOIN 
+    staff_type st ON s.staff_type_id = st.id
+ORDER BY 
+    st.type_name DESC;
+"""
+
+cur.execute(query)
+for res in cur.fetchall():
+    print(res)
+
+con.close()
