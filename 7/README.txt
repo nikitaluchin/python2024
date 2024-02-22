58a3805043721d5f4d797a715d46ae512ef4d7c0
nikitaluchin <luchinnightwolf543@gmail.com>
2024-02-22 18:52:48 +0300

    Задание 4. Главная страница клиентской части приложения содержит информацию о всех записях главной модели (таблицы) в краткой форме.

--- 7/README.txt
+++ 7/README.txt
@@ -13,6 +13,6 @@ manage.py makemigrations --empty app
 Списки отображения:
 https://developer.mozilla.org/ru/docs/Learn/Server-side/Django/Admin_site#%D0%BD%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B0_%D0%BE%D1%82%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F_%D1%81%D0%BF%D0%B8%D1%81%D0%BA%D0%BE%D0%B2
 
-4. 
+4. app folder: urls.py, views.py
 
 ?. detail view либо доделать, либо убрать
--- 7/hospital/app/views.py
+++ 7/hospital/app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Patient
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    num_patients = Patient.objects.count()
+    patients = Patient.objects.all()
+
+    context = {
+        'num_patients': num_patients,
+        'patients': patients,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
